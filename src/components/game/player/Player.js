ffd9f8973ad84ce56aa393a3d4fd7ed9bd9de608
Safder Areepattamannil <safderareepattamannil@gmail.com>
2021-01-27 22:03:49 -0500

    Figure out how to handle selected element

--- src/components/game/player/Player.js
+++ src/components/game/player/Player.js
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Character from "./character/Character";
 
 export default function Player() {
+    const [ready, setReady] = useState(false);
+    const [team, setTeam] = useState([]);
+    const [selected, setSelected] = useState();
+    let path = "http://localhost:8888";
+
+    let loadTeam = () => {
+        fetch(`http://localhost:8888/char/uzumakiNaruto`)
+            .then((res) => {
+                if (res.status !== 200) {
+                    console.log("Error with GET");
+                    return;
+                }
+                return res.json();
+            })
+            .then((data) => {
+                if (!data) {
+                    console.log("error");
+                }
+                setTeam([...data]);
+                setReady(true);
+                console.log(team);
+            })
+            .catch((err) => console.log(err.message));
+    };
+
+    useEffect(() => {
+        loadTeam();
+    }, []);
+
+
+
     return (
         <div className="player">
-            <div className="player__char">
-                <div className="player__char-wrapper">
-                    <div className="player__char-image">dp</div>
-                    <div className="player__char-health">100</div>
-                </div>
-                <div className="player__char-skills-container">
-                    <div className="skill">skill 1</div>
-                    <div className="skill">skill 2</div>
-                    <div className="skill">skill 3</div>
-                    <div className="skill">skill 4</div>
-                </div>
-            </div>
-            <div className="player__char"></div>
-            <div className="player__char"></div>
+            {team.length > 0 && (
+                <Character
+                    id={0}
+                    path={path}
+                    team={team}
+                    selected={selectedItem}
+                    currSelection={selected}
+                />
+            )}
+            <Character path={path} team={team}/>
+            <Character path={path} team={team}/>
         </div>
     );
 }
---
+++ src/components/game/player/character/Character.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+export default function Character({ id, path, team }) {
+    const [selected, setSelected] = useState();
+    // TODO: FIGURE OUT HOW TO HANDLE SELECT
+    let selectedItem = (e) => {
+        console.log(e.currentTarget.id);
+        setSelected(e.currentTarget.id);
+        console.log(selected);
+    };
+
+    return (
+        <div className="player__char">
+            <div className="player__char-wrapper" >
+                <div
+                    id={`${id}-avatar`}
+                    className={`player__char-image selectable`}
+                    onClick={(e)=>selectedItem(e)}
+                >
+                    <img
+                        src={`${path}/uzumakiNaruto/avatar.jpg`}
+                        alt="avatar"
+                    />
+                </div>
+                <div className="player__char-health">
+                    <span className="hpValue">{team[0].hp}</span>
+                    <progress
+                        id="health"
+                        value={team[0].hp}
+                        max="100"
+                    ></progress>
+                </div>
+            </div>
+            <div className="player__char-skills-container">
+                <div id={`${id}-skill-1`} className="skill selectable">
+                    <img
+                        src={`${path}/uzumakiNaruto/skill1/avatar.jpg`}
+                        alt="skill 1"
+                    />
+                </div>
+                <div className="skill selectable">
+                    <img
+                        src={`${path}/uzumakiNaruto/skill2/avatar.jpg`}
+                        alt="skill 1"
+                    />
+                </div>
+                <div className="skill selectable">
+                    <img
+                        src={`${path}/uzumakiNaruto/skill3/avatar.jpg`}
+                        alt="skill 1"
+                    />
+                </div>
+                <div className="skill selectable">
+                    <img
+                        src={`${path}/uzumakiNaruto/skill4/avatar.jpg`}
+                        alt="skill 1"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
